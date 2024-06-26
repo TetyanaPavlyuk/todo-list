56406c3ec1391d969e0f8fbcc28e7623adce139a
Tetyana Pavlyuk <tetyana.pas@gmail.com>
2024-06-26 12:01:10 +0300

    Add task-list and tag-list into views, urls

--- task/urls.py
+++ task/urls.py
@@ -1,10 +1,11 @@
 from django.urls import path
 
-from views import index
+from .views import TagListView, TaskListView
 
 
 urlpatterns = [
-    path("", index, name = index),
+    path("", TaskListView.as_view(), name="index"),
+    path("tags/", TagListView.as_view(), name="tag-list"),
 ]
 
 app_name = "task"
--- task/views.py
+++ task/views.py
@@ -1,3 +1,15 @@
-from django.shortcuts import render
+from django.contrib.auth.mixins import LoginRequiredMixin
+from django.views import generic
 
-# Create your views here.
+from task.models import Tag, Task
+
+
+class TagListView(LoginRequiredMixin, generic.ListView):
+    model = Tag
+    context_object_name = "tag_list"
+
+
+class TaskListView(LoginRequiredMixin, generic.ListView):
+    model = Task
+    queryset = Task.objects.prefetch_related("tags")
+    context_object_name = "task_list"
